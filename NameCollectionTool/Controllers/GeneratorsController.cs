ffb0e848accb64cc1f0bcde20983eec746955f7c
Gum-dwalen <95374174+Gum-dwalen@users.noreply.github.com>
2022-07-01 23:34:21 +0200

    Added structure and routing and nav for generators view

---
+++ NameCollectionTool/Controllers/GeneratorsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NameCollectionTool.Controllers
+{
+    public class GeneratorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
